695c2c1fa6dedb112da3408bf8a88ed05e5189ec
EMME-ESSE <matimancini@gmail.com>
2022-12-06 06:57:49 -0800

    Functions for strings

--- strings.c
+++ strings.c
@@ -23,7 +23,7 @@ int _strlen(char *string)
 char *_strncpy(char *dest, char *src, int n)
 {
 	int index = 0, src_len = 0;
-	
+
 	while (src[index++])
 		src_len++;
 	for (index = 0; src[index] && index < n; index++)

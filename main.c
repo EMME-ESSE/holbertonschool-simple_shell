8fccdcab3fe611ac09c0b9d737f5529126ba78cf
EMME-ESSE <matimancini@gmail.com>
2022-12-09 06:03:18 -0800

    Shell program

--- main.c
+++ main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 int main(int ac, char **argv)
 {
 	char *prompt = "(Hshell) $ ";
@@ -12,34 +11,42 @@ int main(int ac, char **argv)
 	int i;
 	(void)ac;
 
-    /* Create a loop for the shell's prompt */
 	while (1)
 	{
 		printf("%s", prompt);
 		if (isatty(STDIN_FILENO))
 			write(STDOUT_FILENO, "\n", 1);
+		line1 = malloc(sizeof(char) * n);
+		if (!line1)
+		{
+			free(line1);
+			return(-1);
+		}
 		chars_n = getline(&line1, &n, stdin);
-	        /* check if the getline function failed or user use CTRL + D */ 
-		
 		if (chars_n == -1)
 		{
-			printf("Exiting Shell...\n");	
+			printf("Exiting Shell...\n");
+			free(line1);
 			return(-1);
      		}  
-       	 /* allocate space for a copy of the input */
 		line2 = malloc(sizeof(char *) * chars_n + 1);
 		if (line2 == NULL)
 		{
 			perror("Memory allocation error");
+			free(line2);
 			return (-1);
 		}
 		strcpy(line2, line1);
-               /* splits the array and calculates the total number of tokens */
 		token = strtok(line1, delim);
 		while (token != NULL)
 		{
 			num_tokens++;
 			token = strtok(NULL, delim);
+		}
+		if (!token)
+		{
+			free(line1);
+			return(-1);
 		}
 		num_tokens++;
 		argv = malloc(sizeof(char *) * num_tokens + 1);
@@ -48,14 +55,19 @@ int main(int ac, char **argv)
 			perror("Memory allocation error");
 			return(-1);
 		}
-        /* Store each token in argv */
 		token = strtok(line2, delim);
+		if (!token)
+		{
+			free(token);
+			return(-1);
+		}
 		for (i = 0; token != NULL; i++)
 		{
 			argv[i] = malloc(sizeof(char *) * strlen(token) + 1);
 			if (argv[i] == NULL)
 			{
 				perror("Memory allocation error");
+				free(argv[i]);
 				return(-1);
 			}
 			strcpy(argv[i], token);
@@ -63,10 +75,10 @@ int main(int ac, char **argv)
 		}
 		argv[i] = NULL;
        		__launch(argv);
+		free(argv[i]);
+		free(argv);
+		free(line2);
+		free(line1);
 	}
-	free(argv[i]);
-	free(argv);
-	free(line2);
-	free(line1);
 	return (0);
 }
